cfa38cbd24f98b0c5578e27dea3222b93164eaf7
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2024-10-10 18:34:57 -0300

    feat: ensure Login will not load spinner and error on start

---
+++ src/presentation/pages/login/login.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import styles from './login.module.scss';
+import { Footer, Header, Input, FormStatus } from '@/presentation/components';
+import Context from '@/presentation/contexts/form/form.context';
+
+type StateProps = {
+  isLoading: boolean
+  errorMessage: string
+};
+
+const Login: React.FC = () => {
+  const [state] = useState<StateProps>({
+    isLoading: false,
+    errorMessage: ''
+  });
+
+  return (
+    <div className={styles.login}>
+      <Header />
+      <Context.Provider value={state}>
+        <form className={styles.form}>
+          <h2>Login</h2>
+          <Input type="email" name="email" placeholder="Digite seu e-mail" />
+          <Input type="password" name="password" placeholder="Digite sua senha" />
+          <button className={styles.submit} type="submit">Entrar</button>
+          <span className={styles.link}>Criar conta</span>
+          <FormStatus />
+        </form>
+      </Context.Provider>
+      <Footer />
+    </div>
+  );
+};
+
+export default Login;
