5543b472d7aef39814727362ac3199b2d9501644
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2024-10-19 10:37:09 -0300

    feat: create MinLengthValidation

---
+++ src/validation/validators/min-length/min-length.validation.test.ts
@@ -0,0 +1,22 @@
+import { MinLengthValidation } from './min-length.validation';
+import { InvalidFieldError } from '@/validation/errors';
+import faker from 'faker';
+
+const createSut = (): MinLengthValidation => {
+  return new MinLengthValidation(faker.database.column(), 5);
+};
+
+describe('MinLengthValidation', () => {
+
+  it('Should return an error when the field is lesser than the minimum length', () => {
+    const sut = createSut();
+    const error = sut.validate(faker.random.alphaNumeric(4));
+    expect(error).toEqual(new InvalidFieldError());
+  });
+
+  test('Should return false when the field is greater than the minimum length', () => {
+    const sut = createSut();
+    const error = sut.validate(faker.random.alphaNumeric(5));
+    expect(error).toBeFalsy();
+  });
+});
