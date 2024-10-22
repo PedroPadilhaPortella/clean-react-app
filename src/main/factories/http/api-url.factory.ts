f97062f52a2ac97850456eeed3e21d64fda86ae5
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2024-10-22 10:19:52 -0300

    refactor: move API_URL to a env variable

---
+++ src/main/factories/http/api-url.factory.ts
@@ -0,0 +1,3 @@
+export const apiUrlFactory = (path: string): string => {
+  return `process.env.API_URL/${path}`;
+};
--- src/main/factories/usecases/authentication/remote-authentication.factory.ts
+++ src/main/factories/usecases/authentication/remote-authentication.factory.ts
@@ -1,9 +1,10 @@
 import { RemoteAuthentication } from '@/data/usecases';
 import { Authentication } from '@/domain/usecases';
 import { AxiosHttpClient } from '@/infra/http-client';
+import { apiUrlFactory } from '@/main/factories/http/api-url.factory';
 
 export const remoteAuthenticationFactory = (): Authentication => {
-  const url = 'http://localhost:5000/api/login';
+  const url = apiUrlFactory('/login');
   const axiosHttpClient = new AxiosHttpClient();
   return new RemoteAuthentication(url, axiosHttpClient);
 };
