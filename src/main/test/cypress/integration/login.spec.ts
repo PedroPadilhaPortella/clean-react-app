dd6ebff37e99462ecad6bca93b266e006b6b3e0f
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2024-11-08 18:01:25 -0300

    test: ensure Login present valid state if form is valid

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -34,4 +34,36 @@ describe('Login', () => {
     cy.getByTestId('submit').should('have.attr', 'disabled');
     cy.getByTestId('error-wrap').should('not.have.descendants');
   });
+
+  it('Should present valid state when form is valid', () => {
+    cy.getByTestId('email').type(faker.internet.email());
+    cy.getByTestId('password').type(faker.random.alphaNumeric(6));
+
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Tudo certo!')
+      .should('contain.text', '🟢');
+
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Tudo certo!')
+      .should('contain.text', '🟢');
+
+    cy.getByTestId('submit').should('not.have.attr', 'disabled');
+    cy.getByTestId('error-wrap').should('not.have.descendants');
+  });
+
+  it('Should present error when invalid credentials are provided', () => {
+    cy.getByTestId('email').type(faker.internet.email());
+    cy.getByTestId('password').type(faker.random.alphaNumeric(6));
+
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Tudo certo!')
+      .should('contain.text', '🟢');
+
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Tudo certo!')
+      .should('contain.text', '🟢');
+
+    cy.getByTestId('submit').should('not.have.attr', 'disabled');
+    cy.getByTestId('error-wrap').should('not.have.descendants');
+  });
 });
