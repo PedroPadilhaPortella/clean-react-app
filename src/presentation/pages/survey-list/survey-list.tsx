74cd4b9043e2bf680a85f365547975651e246284
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2024-11-16 19:33:17 -0300

    refactor: style mobile layout

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -23,6 +23,42 @@ const SurveyList: React.FC = () => {
             </div>
             <footer>Ver resultado</footer>
           </li>
+          <li>
+            <div className={styles.surveyContent}>
+              <Icon iconName={IconName.thumbDown} />
+              <time>
+                <span className={styles.day}>16</span>
+                <span className={styles.month}>11</span>
+                <span className={styles.year}>2024</span>
+              </time>
+              <p>Qual seu framework web favorito?</p>
+            </div>
+            <footer>Ver resultado</footer>
+          </li>
+          <li>
+            <div className={styles.surveyContent}>
+              <Icon iconName={IconName.thumbDown} />
+              <time>
+                <span className={styles.day}>16</span>
+                <span className={styles.month}>11</span>
+                <span className={styles.year}>2024</span>
+              </time>
+              <p>Qual seu framework web favorito?</p>
+            </div>
+            <footer>Ver resultado</footer>
+          </li>
+          <li>
+            <div className={styles.surveyContent}>
+              <Icon iconName={IconName.thumbDown} />
+              <time>
+                <span className={styles.day}>16</span>
+                <span className={styles.month}>11</span>
+                <span className={styles.year}>2024</span>
+              </time>
+              <p>Qual seu framework web favorito?</p>
+            </div>
+            <footer>Ver resultado</footer>
+          </li>
         </ul>
       </div>
       <Footer />
