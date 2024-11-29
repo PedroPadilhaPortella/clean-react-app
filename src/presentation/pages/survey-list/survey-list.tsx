ea2983a81582d43b0f2be16a4848e0212e09edba
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2024-11-29 15:21:51 -0300

    refactor: change SurveyList to use custom hook

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,21 +1,20 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { SurveyListItem, Error, SurveyContext } from '@/presentation/pages/survey-list/components';
+import { Error, SurveyContext, SurveyListItem } from '@/presentation/pages/survey-list/components';
 import { Footer, Header } from '@/presentation/components';
+import { useErrorHandler } from '@/presentation/hooks';
 import { LoadSurveyList } from '@/domain/usecases';
 
 import styles from './survey-list.module.scss';
-import { AccessDeniedError } from '@/domain/errors';
-import { useHistory } from 'react-router-dom';
-import { ApiContext } from '@/presentation/contexts';
 
 type Props = {
   loadSurveyList: LoadSurveyList
 };
 
 const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
-  const history = useHistory();
-  const { setCurrentAccount } = useContext(ApiContext);
+  const errorHandler = useErrorHandler((error: Error) => {
+    setState({ ...state, error: error.message });
+  });
 
   const [state, setState] = useState({
     surveys: [] as LoadSurveyList.Model[],
@@ -27,14 +26,7 @@ const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
     (async function () {
       await loadSurveyList.load()
         .then((surveys) => setState({ ...state, surveys }))
-        .catch((error) => {
-          if (error instanceof AccessDeniedError) {
-            setCurrentAccount(undefined);
-            history.replace('/login');
-          } else {
-            setState({ ...state, error: error.message });
-          }
-        });
+        .catch((error) => errorHandler(error));
     })();
   }, [state.reload]);
 
