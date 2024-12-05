351673c5bce9737bf830b9aecef34dea2ea66259
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2024-12-05 18:25:58 -0300

    feat: create LoadSurveyResult usecase

--- src/domain/usecases/index.ts
+++ src/domain/usecases/index.ts
@@ -1,3 +1,4 @@
 export { Authentication } from './authentication';
 export { RegisterAccount } from './register-account';
 export { LoadSurveyList } from './load-survey-list';
+export { LoadSurveyResult } from './load-survey-result';
